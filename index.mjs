c4c52eb17e54b34aef6ab926bb62beda39363560
Jordan Harband <ljharb@gmail.com>
2024-09-19 14:34:27 -0700

    [Refactor] remove useless ESM

--- index.mjs
+++
@@ -1,7 +0,0 @@
-import getPolyfill from 'es-set/polyfill';
-
-export default getPolyfill();
-
-export { default as getPolyfill } from 'es-set/polyfill';
-export { default as implementation } from 'es-set/implementation';
-export { default as shim } from 'es-set/shim';
